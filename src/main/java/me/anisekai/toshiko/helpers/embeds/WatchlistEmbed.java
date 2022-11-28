f3e0bc5f4125e953f0de6ff9a1521bbf54c7a3ae
alexpado <contact@alexpado.fr>
2022-11-28 01:28:20 +0100

    feat(watchlist): Add macro pouet display

--- src/main/java/me/anisekai/toshiko/helpers/embeds/WatchlistEmbed.java
+++ src/main/java/me/anisekai/toshiko/helpers/embeds/WatchlistEmbed.java
@@ -4,17 +4,17 @@ import fr.alexpado.jda.interactions.responses.ButtonResponse;
 import fr.alexpado.jda.interactions.responses.SlashResponse;
 import me.anisekai.toshiko.entities.Anime;
 import me.anisekai.toshiko.entities.Watchlist;
-import me.anisekai.toshiko.helpers.containers.VariablePair;
-import me.anisekai.toshiko.utils.DiscordUtils;
+import me.anisekai.toshiko.helpers.embeds.watchlist.WatchlistAnimePart;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.utils.messages.MessageRequest;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WatchlistEmbed implements SlashResponse, ButtonResponse {
 
@@ -38,34 +38,33 @@ public class WatchlistEmbed implements SlashResponse, ButtonResponse {
 
         return (amb) -> {
             EmbedBuilder builder = new EmbedBuilder();
-            List<Anime>  animes  = new ArrayList<>(this.watchlist.getAnimes());
-            animes.sort(Anime::compareTo);
+            List<WatchlistAnimePart> animes = this.watchlist.getAnimes().stream()
+                                                            .sorted()
+                                                            .map(anime -> new WatchlistAnimePart(anime, this.scores.getOrDefault(anime, 0.0)))
+                                                            .toList();
 
             builder.setAuthor(String.format("%s (%s)", this.watchlist.getStatus().getDisplay(), animes.size()));
 
-            StringBuilder withLinks    = new StringBuilder();
-            StringBuilder withoutLinks = new StringBuilder();
-
-            for (Anime anime : animes) {
-
-                VariablePair<String, String> result = DiscordUtils.buildAnimeList(anime, this.scores.getOrDefault(anime, 0.0));
-
-                String entryWithLink    = result.getFirst();
-                String entryWithoutLink = result.getSecond();
-
-                withLinks.append(entryWithLink).append("\n\n");
-                withoutLinks.append(entryWithoutLink).append("\n\n");
-            }
-
-            if (withLinks.length() > MessageEmbed.DESCRIPTION_MAX_LENGTH) {
-                builder.setDescription(withoutLinks);
+            if (animes.size() > 90) {
+                builder.setDescription("*Il faudrait ptet arrêter d'ajouter des animes à ce niveau là hein...*");
+            } else if (animes.size() > 40) {
+                builder.setDescription(animes.stream()
+                                             .map(WatchlistAnimePart::getMacroFormat)
+                                             .collect(Collectors.joining("\n")));
             } else {
-                builder.setDescription(withLinks);
+                String full  = animes.stream().map(WatchlistAnimePart::getFullFormat).collect(Collectors.joining("\n"));
+                String small = animes.stream().map(WatchlistAnimePart::getSmallFormat).collect(Collectors.joining("\n"));
+                String macro = animes.stream().map(WatchlistAnimePart::getMacroFormat).collect(Collectors.joining("\n"));
+
+                String output = Stream.of(full, small, macro)
+                                      .filter(opt -> opt.length() < MessageEmbed.DESCRIPTION_MAX_LENGTH)
+                                      .findFirst()
+                                      .orElse("*Il faudrait ptet arrêter d'ajouter des animes à ce niveau là hein...*");
+
+                builder.setDescription(output);
             }
 
-            builder.setFooter("Dernière actualisation le");
-            builder.setTimestamp(LocalDateTime.now());
-
+            builder.setTimestamp(ZonedDateTime.now());
             amb.setEmbeds(builder.build());
         };
     }
