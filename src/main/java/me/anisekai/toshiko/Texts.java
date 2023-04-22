6529c7cbfca6880e7edcc2750eb6a1aa2001df2e
alexpado <contact@alexpado.fr>
2023-04-22 18:13:22 +0200

    feat(texts): Add icon for admin-reserved commands and options

--- src/main/java/me/anisekai/toshiko/Texts.java
+++ src/main/java/me/anisekai/toshiko/Texts.java
@@ -13,7 +13,7 @@ import java.util.Locale;
 public final class Texts {
 
     // <editor-fold desc="@ anime/announce">
-    public static final String ANIME_NOTIFY_ANNOUNCE__DESCRIPTION  = "Pour un ou plusieurs animes, envoi ou met à jour le message d'annonce.";
+    public static final String ANIME_NOTIFY_ANNOUNCE__DESCRIPTION  = "\uD83D\uDD12 Pour un ou plusieurs animes, envoi ou met à jour le message d'annonce.";
     public static final String ANIME_NOTIFY_ANNOUNCE__OPTION_ANIME = "Nom de l'anime";
     // </editor-fold>
 
@@ -23,13 +23,13 @@ public final class Texts {
     // </editor-fold>
 
     // <editor-fold desc="@ anime/status">
-    public static final String ANIME_STATUS__DESCRIPTION   = "Change le statut d'un anime.";
+    public static final String ANIME_STATUS__DESCRIPTION   = "\uD83D\uDD12 Change le statut d'un anime.";
     public static final String ANIME_STATUS__OPTION_ANIME  = "Nom de l'anime";
     public static final String ANIME_STATUS__OPTION_STATUS = "Nouveau statut de l'anime";
     // </editor-fold>
 
     // <editor-fold desc="@ anime/interest">
-    public static final String ANIME_INTEREST__DESCRIPTION                         = "Change ton niveau d'interêt pour un anime";
+    public static final String ANIME_INTEREST__DESCRIPTION                         = "\uD83E\uDE99 Change ton niveau d'interêt pour un anime";
     public static final String ANIME_INTEREST__OPTION_NAME                         = "Nom de l'anime";
     public static final String ANIME_INTEREST__OPTION_LEVEL                        = "Niveau de l'intérêt";
     public static final String ANIME_INTEREST__OPTION_LEVEL__CHOICE_INTERESTED     = "Je suis intéressé";
@@ -38,14 +38,14 @@ public final class Texts {
     // </editor-fold>
 
     // <editor-fold desc="@ anime/progress">
-    public static final String ANIME_PROGRESS__DESCRIPTION    = "Change la progression de visionnage d'un anime";
+    public static final String ANIME_PROGRESS__DESCRIPTION    = "\uD83D\uDD12 Change la progression de visionnage d'un anime";
     public static final String ANIME_PROGRESS__OPTION_ANIME   = "Nom de l'anime";
     public static final String ANIME_PROGRESS__OPTION_WATCHED = "Nombre d'épisodes visionnés";
     public static final String ANIME_PROGRESS__OPTION_AMOUNT  = "Nombre total d'épisodes";
     // </editor-fold>
 
     // <editor-fold desc="@ anime/import">
-    public static final String ANIME_IMPORT__DESCRIPTION = "Importe un anime depuis une chaine JSON.";
+    public static final String ANIME_IMPORT__DESCRIPTION = "\uD83E\uDE99 Importe un anime depuis une chaine JSON.";
     public static final String ANIME_IMPORT__OPTION_JSON = "JSON à importer";
     // </editor-fold>
 
@@ -62,7 +62,7 @@ public final class Texts {
     // </editor-fold>
 
     // <editor-fold desc="@ schedule/daily">
-    public static final String SCHEDULE_DAILY__DESCRIPTION     = "Programme des soirées anime de façon journalière";
+    public static final String SCHEDULE_DAILY__DESCRIPTION     = "\uD83D\uDD12 Programme des soirées anime de façon journalière";
     public static final String SCHEDULE_DAILY__OPTION_ANIME    = "Anime à programmer";
     public static final String SCHEDULE_DAILY__OPTION_AT       = "Heure à laquelle les soirées anime doivent être programmées tous les jours (HH:MM)";
     public static final String SCHEDULE_DAILY__OPTION_AMOUNT   = "Nombre d'épisode à regarder à chaque scéance";
@@ -70,7 +70,7 @@ public final class Texts {
     // </editor-fold>
 
     // <editor-fold desc="@ schedule/weekly">
-    public static final String SCHEDULE_WEEKLY__DESCRIPTION     = "Programme des soirées anime de façon hebdomadaire";
+    public static final String SCHEDULE_WEEKLY__DESCRIPTION     = "\uD83D\uDD12 Programme des soirées anime de façon hebdomadaire";
     public static final String SCHEDULE_WEEKLY__OPTION_ANIME    = "Anime à programmer";
     public static final String SCHEDULE_WEEKLY__OPTION_AT       = "Heure à laquelle les soirées anime doivent être programmées toutes les semaines (HH:MM)";
     public static final String SCHEDULE_WEEKLY__OPTION_AMOUNT   = "Nombre d'épisode à regarder à chaque scéance";
@@ -78,7 +78,7 @@ public final class Texts {
     // </editor-fold>
 
     // <editor-fold desc="@ schedule/anime">
-    public static final String SCHEDULE_ANIME__DESCRIPTION   = "Programme une soirée anime";
+    public static final String SCHEDULE_ANIME__DESCRIPTION   = "\uD83D\uDD12 Programme une soirée anime";
     public static final String SCHEDULE_ANIME__OPTION_ANIME  = "Anime à programmer";
     public static final String SCHEDULE_ANIME__OPTION_AT     = "Heure à laquelle la soirée anime doit être programmée (HH:MM)";
     public static final String SCHEDULE_ANIME__OPTION_AMOUNT = "Nombre d'épisode à regarder pour la scéance";
@@ -86,11 +86,11 @@ public final class Texts {
     // </editor-fold>
 
     // <editor-fold desc="@ schedule/calibrate">
-    public static final String SCHEDULE_CALIBRATE__DESCRIPTION = "Recalibre tous les évènements programmés";
+    public static final String SCHEDULE_CALIBRATE__DESCRIPTION = "\uD83D\uDD12 Recalibre tous les évènements programmés";
     // </editor-fold>
 
     // <editor-fold desc="@ schedule/delay">
-    public static final String SCHEDULE_DELAY__DESCRIPTION  = "Décale les évènements des 6 prochaines heures";
+    public static final String SCHEDULE_DELAY__DESCRIPTION  = "\uD83D\uDD12 Décale les évènements des 6 prochaines heures";
     public static final String SCHEDULE_DELAY__OPTION_DELAY = "Temps (en minute) de décalage à appliquer aux évènements";
     // </editor-fold>
 
@@ -100,12 +100,20 @@ public final class Texts {
     // </editor-fold>
 
     // <editor-fold desc="@ refresh">
-    public static final String REFRESH__DESCRIPTION                     = "Lance une actualisation sur une partie spécifique.";
+    public static final String REFRESH__DESCRIPTION                     = "\uD83D\uDD12 Lance une actualisation sur une partie spécifique.";
     public static final String REFRESH__OPTION_TARGET                   = "Cible de l'actualisation";
     public static final String REFRESH__OPTION_TARGET__CHOICE_WATCHLIST = "Listes de visionnage";
     public static final String REFRESH__OPTION_TARGET__CHOICE_ANNOUNCE  = "Message d'annonces (déjà envoyés uniquement)";
     public static final String REFRESH__OPTION_TARGET__CHOICE_SCHEDULE  = "Évènements du serveur";
     public static final String REFRESH__OPTION_FORCE                    = "(Listes de visionnage uniquement) Force l'actualisation des messages, même s'ils n'existent pas.";
+    // </editor-fold>
+
+    // <editor-fold desc="@ profile">
+    public static final String PROFILE_DESCRIPTION    = "Mettre à jour les données d'un profil utilisateur";
+    public static final String PROFILE__OPTION_USER   = "\uD83D\uDD12 Utilisateur pour lequel le profil sera actualisé. Ne pas préciser pour modifier ses propres informations.";
+    public static final String PROFILE__OPTION_ICON   = "Change l'icône de vote";
+    public static final String PROFILE__OPTION_ACTIVE = "\uD83D\uDD12 Change le statut d'activité";
+    public static final String PROFILE__OPTION_ADMIN  = "\uD83D\uDD12 Change le statut administrateur";
     // </editor-fold>
 
     private Texts() {}
